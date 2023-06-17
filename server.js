198e80b3865c2504187bcf5faf76752a3d518f30
Татьяна Яблонская <t.n.yablonskaya@gmail.com>
2023-06-17 20:09:57 +0300

    add fix

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const mongoose = require("mongoose");
 const app = require("./app");
-const { DB_HOST } = require("./config");
+const { DB_HOST } = process.env;
+// const { DB_HOST } = require("./config");
 
-// const PORT = 3000;
-// mongoose.set("strictQuery", true);
+mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(3000))
-  .catch((error) => console.log(error.message), process.exit(1));
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
-
-// <!-- xhD3by4Te4.v7!* -->
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
