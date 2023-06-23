26ee92097965ee73d34a960a1e9657053e2ede9c
Татьяна Яблонская <t.n.yablonskaya@gmail.com>
2023-06-23 19:19:22 +0300

    add decision

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,10 +3,12 @@ const register = require("./register");
 const login = require("./login");
 const getCurrent = require("./getCurrent");
 const logout = require("./logout");
+const updateAvatar = require("./updateAvatar");
 
 module.exports = {
   register: controllerWraper(register),
   login: controllerWraper(login),
   getCurrent: controllerWraper(getCurrent),
   logout: controllerWraper(logout),
+  updateAvatar: controllerWraper(updateAvatar),
 };
--- middlewars/index.js
+++ middlewars/index.js
@@ -1,9 +1,11 @@
 const validataBody = require("./validateBody");
 const isValidId = require("./isValidId");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   validataBody,
   isValidId,
   authenticate,
+  upload,
 };
